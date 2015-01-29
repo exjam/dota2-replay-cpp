c113dbbac2084b532d0cbf91e5767bccd49e8b72
James Benton <james.benton2@gmail.com>
2015-01-29 14:06:48 +0000

    configure.bat 3 space indent

--- configure.bat
+++ configure.bat
@@ -8,19 +8,18 @@ call set file_list=
 set /A i=0
 
 for /F "usebackq delims=" %%a in ("%file%") do (
-  set /A i+=1
-  call set file_list=%%file_list%% %%a.proto
-  call set proto_files[%%i%%]=%%a
-  call set num_proto_files=%%i%%
+   set /A i+=1
+   call set file_list=%%file_list%% %%a.proto
+   call set proto_files[%%i%%]=%%a
+   call set num_proto_files=%%i%%
 )
 
 REM Download proto files
 call echo Downloading latest dota protobuf files
-
 for /L %%i in (1, 1, %num_proto_files%) do (
-  call set file=%%proto_files[%%i]%%
-  call echo - %%proto_dir%%\%%file%%.proto
-  call powershell -Command "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/SteamRE/SteamKit/master/Resources/Protobufs/dota/%%file%%.proto', '%%proto_dir%%\%%file%%.proto')"
+   call set file=%%proto_files[%%i]%%
+   call echo - %%proto_dir%%\%%file%%.proto
+   call powershell -Command "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/SteamRE/SteamKit/master/Resources/Protobufs/dota/%%file%%.proto', '%%proto_dir%%\%%file%%.proto')"
 )
 
 REM Copy protobuf descriptor
